6553965bad8ccf942e8953b3123a5b9c45b455f6
JoaoPito <joao.pito@hotmail.com>
2024-04-20 18:34:34 -0300

    Implemented MessageCommand and ResetCommand

--- app/commands.py
+++ app/commands.py
@@ -1,7 +1,18 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+from app.app import AyanamiApp
 from models.command_base import CommandBase
 
 class MessageCommand(CommandBase):
-    pass
+    def __init__(self, app: AyanamiApp):
+        super().__init__("msg",)
+        self.app = app
+
+    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        print(context)
+        text_caps = update.message.text.upper()
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
 class ResetCommand(CommandBase):
-    pass
+    async def handle(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await context.bot.send_message(chat_id=update.effective_chat.id, text='RESET')
