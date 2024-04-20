20be7774df329c5d5b246a46ac4bd86707665716
JoaoPito <joao.pito@hotmail.com>
2024-04-20 14:07:35 -0300

    implemented stubs for app testing

--- tests/ai/stubs.py
+++ tests/ai/stubs.py
@@ -1,5 +1,10 @@
 from ai.ai_interface import AIInterface
 
 class EmptyAIStub(AIInterface):
+    has_run = False
+
     def __init__(self):
         pass
+
+    def run(self):
+        self.has_run = True
--- tests/chat/stubs.py
+++ tests/chat/stubs.py
@@ -1,6 +1,20 @@
+import asyncio
 from chat.chat_interface import ChatInterface
 
-
 class EmptyChatStub(ChatInterface):
+    filters = {"message": "msg"}
+    has_called_add_handler = False
+    has_run = False
+    
     def __init__(self):
         pass
+
+    def add_handler(self, name: str, handler):
+        self.has_called_add_handler = True
+        return super().add_handler(name, handler)
+    
+    def run(self):
+        self.has_run = True
+
+    def call_handler(self, name, **kwargs):
+        asyncio.run(self.handlers[name](**kwargs))
