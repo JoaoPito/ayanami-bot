ace75b66078a71db70236dcdca0dfdce2fe6dc26
JoaoPito <joao.pito@hotmail.com>
2024-04-20 12:38:34 -0300

    feat: implemented ChatInterface add_handler

--- chat/chat_interface.py
+++ chat/chat_interface.py
@@ -1,3 +1,9 @@
 class ChatInterface:
+    filters = {}
+    handlers = {}
+
     def __init__(self):
         pass
+
+    def add_handler(self, name:str, handler):
+        self.handlers[name] = handler
