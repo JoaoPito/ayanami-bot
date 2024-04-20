a13a20469e77ba9776dad801c4ef09b866c93201
JoaoPito <joao.pito@hotmail.com>
2024-04-20 17:42:42 -0300

    Refactored main

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from app.app import AyanamiApp
+import os
 from app.app_builder import AyanamiAppBuilder
+from app.commands import MessageCommand, ResetCommand
 import app.tools_loader as tools_loader
 
-from chat.telegram_chat import TelegramChat
-from ai.langchain_ai import LangChainAI
 from auth.token_auth import TokenAuth
 
+from chat.telegramchat_factory import TelegramChatFactory
 import config
 
 config_tools = config.tools
@@ -14,12 +14,19 @@ config_ai_params = config.ai_model
 def main():
     builder = AyanamiAppBuilder()
 
+    chat = TelegramChatFactory().create(os.environ("TELEGRAM_BOT_TOKEN"))
+    builder.add_chat(chat)
+
     ai_tools = tools_loader.load_tools(config_tools)
-    builder.add_ai(LangChainAI(ai_tools, config_ai_params))
-    builder.add_chat(TelegramChat())
+    #ai = LangChainAIFactory().create(ai_tools, config_ai_params)
+    builder.add_ai(ai)
+    
     builder.add_auth(TokenAuth())
     
     app = builder.build_app()
+
+    app.add_message_handler(MessageCommand(app))
+    app.add_command_handler(ResetCommand(app))
 
     app.run()
 
