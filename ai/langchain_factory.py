9deb591ca09be7b6df12b7a0fc390aa1feab79a6
JoaoPito <joao.pito@hotmail.com>
2024-04-27 20:59:39 -0300

    Implemented LangChainAIFactory

---
+++ ai/langchain_factory.py
@@ -0,0 +1,6 @@
+from ai.ai_factory import AIFactory
+from ai.langchain import LangChainAI
+
+class LangChainAIFactory(AIFactory):
+    def create(self, tools, params):
+        return LangChainAI(tools, params)
