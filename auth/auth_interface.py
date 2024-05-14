f5b7b8e7b81eb29fc758a9fa57f1932a932cef16
JoaoPito <joao.pito@hotmail.com>
2024-05-14 15:36:33 -0300

    Updated AuthInterface to allow registering new users

--- auth/auth_interface.py
+++ auth/auth_interface.py
@@ -4,3 +4,6 @@ class AuthInterface:
 
     def is_authorized(self, user_id: int):
         return False
+    
+    def register_user(self, user_id: int, criteria):
+        pass
--- auth/token_auth.py
+++ auth/token_auth.py
@@ -1,5 +1,15 @@
+from uuid import uuid4
 from auth.auth_interface import AuthInterface
 
-
 class TokenAuth(AuthInterface):
-    pass
+    def __init__(self):
+        pass
+
+    def __generate_token__(self):
+        pass
+
+    def is_authorized(self, user_id: int):
+        return False
+    
+    def register_user(self, user_id: int, token: uuid4):
+        raise AttributeError("User is blocked")
