d51863185bfbe4d344c8ff66ccb96a4919b4729b
JoaoPito <joao.pito@hotmail.com>
2024-05-17 19:32:25 -0300

    Implemented Authenticator Interface

--- auth/auth_interface.py
+++ auth/auth_interface.py
@@ -2,8 +2,11 @@ class AuthInterface:
     def __init__(self):
         pass
 
+    def register_new_user(self, user_id):
+        pass
+
+    def try_authorize_user(self, user_id: int, criteria):
+        pass
+
     def is_authorized(self, user_id: int):
         return False
-    
-    def register_user(self, user_id: int, criteria):
-        pass
