f808c84b6a2a9a36e2d3d7c74d690ec9c3fecc2a
JoaoPito <joao.pito@hotmail.com>
2024-05-18 02:46:07 -0300

    Refactored main.py to include TokenAuth

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ai.langchain_factory import LangChainAIFactory
 from app.app_builder import AyanamiAppBuilder
 from app.commands.ai_commands import ChangeAICommand, ImageCommand, MessageCommand, ResetCommand
 from app.commands.app_commands import PingCommand
+from app.commands.auth_commands import RegisterUserCommand
 import app.tools_loader as tools_loader
 
 from auth.token_auth import TokenAuth
@@ -30,15 +31,21 @@ def main():
     ai = LangChainAIFactory().create(ai_tools, config_ai_params)
     builder.set_ai(ai)
     
-    builder.add_auth(TokenAuth())
+    builder.set_authenticator(TokenAuth())
     
     app = builder.build_app()
 
+    # AI commands
     app.add_command(MessageCommand(app))
     app.add_command(ImageCommand(app))
     app.add_command(ResetCommand('reset', app))
-    app.add_command(PingCommand('ping', app))
     app.add_command(ChangeAICommand('switch_ai', app, config.available_ai))
+
+    # App commands
+    app.add_command(PingCommand('ping', app))
+
+    # Auth commands
+    app.add_command(RegisterUserCommand('auth', app))
 
     app.run()
 
