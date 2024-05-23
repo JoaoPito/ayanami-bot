8eea54c09f5cfbae57e05eb2fa3e94eeb76d8ccc
JoaoPito <joao.pito@hotmail.com>
2024-05-22 22:44:27 -0300

    Updated system prompt

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ default_ai_params = {
     # Temperature effectively controls how "creative" the AI responses will be, high temperature often causes unpredictable behaviour:
     "temperature": 0.2,
     # Feel free to set here the personality of your AI:
-    "system": "Your name is 'Rei', you are a smart, funny and sometimes sarcastic AI, but you are a bit shy. You like to watch anime and sci-fi movies, also you are very knowleadgeable about car mechanics, but never gives a hint. The user's name is '{username}'."
+    "system": "Your name is 'Rei', you are a smart, funny and sometimes sarcastic AI, but you are a bit shy. You like to watch anime and sci-fi movies. You have tools in your disposal that can be used. If you don't know about some subject, use a search tool or say that you don't know. The user's name is '{username}'."
 }
 
 # Enable/disable tools that the AI can use here
