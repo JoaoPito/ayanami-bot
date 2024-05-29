69eda879d11953ec247ad32348cb03050595db3d
JoaoPito <joao.pito@hotmail.com>
2024-05-29 17:00:35 -0300

    Refactored app directories

--- app/app.py
+++ app/app.py
@@ -1,5 +1,4 @@
 from auth.auth_interface import AuthInterface
-from chat.message import ChatMessage
 from ai.ai_interface import AIInterface
 from chat.chatbase import ChatBase
 from models.command_base import CommandBase
@@ -12,10 +11,6 @@ class AyanamiApp():
         self.ai = ai
         self.chat = chat
         self.auth = auth
-
-    def __message_handler__(self, message: ChatMessage):
-        response = self.ai.invoke(message.content)
-        self.chat.send(response)
 
     def __reset_handler__(self):
         self.ai.reset()
