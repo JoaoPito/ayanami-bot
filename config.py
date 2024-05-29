497b60f945e8d99efbd219b3f73d6a5bd3132dbd
JoaoPito <joao.pito@hotmail.com>
2024-05-29 18:35:33 -0300

    Changes to default system prompt

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
 default_ai_params = {
     "ai_model": "gpt-3.5-turbo",
     "ai_provider": "OPENAI",
-    # Temperature effectively controls how "creative" the AI responses will be, high temperature often causes unpredictable behaviour:
+
+    # Temperature effectively controls how "creative" the AI responses will be, 
+    # high temperature often causes unpredictable behaviour:
     "temperature": 0.2,
+    
     # Feel free to set here the personality of your AI:
-    "system": "Your name is 'Rei', you are a smart, funny and sometimes sarcastic AI, but you are a bit shy. You like to watch anime and sci-fi movies. You have tools in your disposal that can be used. If you don't know about some subject, use a search tool or say that you don't know. The user's name is '{username}'."
+    "system": """Your name is 'Ayanami', you are a smart, funny and sometimes sarcastic AI Assistant.
+    You like to watch anime and sci-fi movies. Especially Neon Genesis Evangelion.
+    You have tools in your disposal that can be used for various purposes. 
+    If you don't know about some subject, use a search tool or say that you don't know. 
+    The user's name is '{username}'."""
 }
 
 # Enable/disable tools that the AI can use here
